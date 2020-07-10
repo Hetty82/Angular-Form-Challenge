ed305fdcca15dac9305ee2a7bf76ae19cf3a5009
Hetty de Vries <hetty82@gmail.com>
2020-07-10 14:35:00 +0200

    ✨ Setup empty components and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { SignUpFormComponent } from './sign-up-form/sign-up-form.component';
 
 @NgModule({
-    declarations: [AppComponent],
+    declarations: [AppComponent, SignUpComponent, SignUpFormComponent],
     imports: [BrowserModule, AppRoutingModule],
     providers: [],
     bootstrap: [AppComponent],
