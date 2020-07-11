8a829ee610eed5bdb1e75a949ec2ecac33940990
Hetty de Vries <hetty82@gmail.com>
2020-07-11 22:22:15 +0200

    ✨ Add validations

--- src/app/sign-up-form/sign-up-form.component.ts
+++ src/app/sign-up-form/sign-up-form.component.ts
@@ -15,4 +15,24 @@ export class SignUpFormComponent implements OnInit {
     constructor() {}
 
     ngOnInit(): void {}
+
+    // I don't like getters. They hide that they are functions, called on each change detection
+    // cycle. However these are very light, and therefor acceptable. And I wanted to show you I
+    // know I can use getters.
+
+    public get firstName() {
+        return this.form.get('firstName');
+    }
+    public get lastName() {
+        return this.form.get('lastName');
+    }
+    public get email() {
+        return this.form.get('email');
+    }
+    public get password() {
+        return this.form.get('password');
+    }
+    public get passwordConfirmation() {
+        return this.form.get('passwordConfirmation');
+    }
 }
--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import {
+    AbstractControl,
+    FormBuilder,
+    FormGroup,
+    ValidationErrors,
+    ValidatorFn,
+    Validators,
+} from '@angular/forms';
 import { SignUpData } from '../models/sign-up.model';
 import { SignUpService } from '../services/sign-up.service';
 
@@ -11,16 +18,10 @@ import { SignUpService } from '../services/sign-up.service';
 export class SignUpComponent implements OnInit {
     public form: FormGroup;
 
-    constructor(private signUpService: SignUpService) {}
+    constructor(private signUpService: SignUpService, private fb: FormBuilder) {}
 
     ngOnInit(): void {
-        this.form = new FormGroup({
-            firstName: new FormControl(''),
-            lastName: new FormControl(''),
-            email: new FormControl(''),
-            password: new FormControl(''),
-            passwordConfirmation: new FormControl(''),
-        });
+        this.buildForm();
     }
 
     public signUp({ firstName, lastName, email }: SignUpData) {
@@ -30,5 +31,88 @@ export class SignUpComponent implements OnInit {
             lastName: lastName.trim(),
             email: email.trim(),
         });
+    }
+
+    private buildForm() {
+        this.form = this.fb.group(
+            {
+                firstName: [null, Validators.required],
+                lastName: [null, Validators.required],
+                email: [null, [Validators.required, Validators.email]],
+                password: [
+                    null,
+                    [
+                        // 1. Password Field is Required
+                        Validators.required,
+                        // 2. Has a minimum length of 8 characters
+                        Validators.minLength(8),
+                        // 3. check whether the entered password has upper case letter
+                        this.createPatternValidator(/[A-Z]/, { hasCapitalCase: true }),
+                        // 4. check whether the entered password has a lower-case letter
+                        this.createPatternValidator(/[a-z]/, { hasLowerCase: true }),
+                    ],
+                ],
+                passwordConfirmation: [null, Validators.required],
+            },
+            {
+                validators: [
+                    this.validatePasswordConfirmation,
+                    this.validateNoFirstNameInPassword,
+                    this.validatNoLastNameInPassword,
+                ],
+            },
+        );
+    }
+
+    // This was inspired here:
+    // https://codinglatte.com/posts/angular/cool-password-validation-angular/
+    private createPatternValidator(regex: RegExp, error: ValidationErrors): ValidatorFn {
+        return (control: AbstractControl): { [key: string]: any } => {
+            if (!control.value) {
+                return null;
+            }
+            const valid = regex.test(control.value);
+
+            return valid ? null : error;
+        };
+    }
+
+    private validatePasswordConfirmation(group: FormGroup) {
+        const password = group.get('password').value;
+        const confirmation = group.get('passwordConfirmation').value;
+
+        return password === confirmation ? null : { notSame: true };
+    }
+
+    private validateNoFirstNameInPassword(group: FormGroup) {
+        const firstNameControlValue: string = group.get('firstName').value;
+        if (!firstNameControlValue) {
+            return null;
+        }
+        const password = group.get('password').value;
+
+        const firstNames = firstNameControlValue.trim().split(' ');
+        const invalid = firstNames.some((name) => {
+            const regex = new RegExp(name, 'i');
+            return regex.test(password);
+        });
+
+        return invalid ? { noFirstNameInPassword: true } : null;
+    }
+
+    private validatNoLastNameInPassword(group: FormGroup) {
+        const lastNameControlValue: string = group.get('lastName').value;
+        if (!lastNameControlValue) {
+            return null;
+        }
+        const password: string = group.get('password').value;
+
+        const lastNames = lastNameControlValue.trim().split(' ');
+        const invalid = lastNames.some((name) => {
+            const regex = new RegExp(name, 'i');
+            return regex.test(password);
+        });
+
+        return invalid ? { noLastNameInPassword: true } : null;
     }
 }
