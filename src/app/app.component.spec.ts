4384b6de919ab0d81198404cd43e1d33f38fd909
Hetty de Vries <hetty82@gmail.com>
2020-07-13 12:49:42 +0200

    ✅ Fix broken app component specs

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed, async } from '@angular/core/testing';
+import { async, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
@@ -16,18 +16,16 @@ describe('AppComponent', () => {
         expect(app).toBeTruthy();
     });
 
-    it(`should have as title 'angular-form-challenge'`, () => {
+    it(`should have as title 'Angular Form Challenge'`, () => {
         const fixture = TestBed.createComponent(AppComponent);
         const app = fixture.componentInstance;
-        expect(app.title).toEqual('angular-form-challenge');
+        expect(app.title).toEqual('Angular Form Challenge');
     });
 
     it('should render title', () => {
         const fixture = TestBed.createComponent(AppComponent);
         fixture.detectChanges();
         const compiled = fixture.nativeElement;
-        expect(compiled.querySelector('.content span').textContent).toContain(
-            'angular-form-challenge app is running!',
-        );
+        expect(compiled.querySelector('h1').textContent).toContain('Angular Form Challenge');
     });
 });
